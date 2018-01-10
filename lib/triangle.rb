f3c800d0b0762d62b0176d2cf8123b817fbbb75e
Brad Hambrice <hambrice@uab.edu>
2018-01-10 21:36:53 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,14 @@
 class Triangle
-  # write code here
+attr_accessor :first_length, :second_length, :third_length
+  def initialize(first_length, second_length, third_length)
+    @first_length = first_length
+    @second_length = second_length
+    @third_length = third_length
+  end
+  
+  def kind 
+end
+
+class TriangleError < StandardError
+  
 end
