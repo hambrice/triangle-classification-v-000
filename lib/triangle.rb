a027ffa80fae7d5f6702b85736313551645324f0
Brad Hambrice <hambrice@uab.edu>
2018-01-10 21:50:17 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,7 @@ attr_accessor :first_length, :second_length, :third_length
 end
 
 def proper_triangle
- if @first_length + @second_length < @third_length && @first_length + @third_length < @second_length && @second_length + @third_length < @first_length
+ if @first_length + @second_length > @third_length && @first_length + @third_length > @second_length && @second_length + @third_length > @first_length
    return true 
  end
  end
