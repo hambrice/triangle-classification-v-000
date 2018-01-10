f1f5e10da3945c5e45ad14c6a1e0923445f5bee0
Brad Hambrice <hambrice@uab.edu>
2018-01-10 21:44:07 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,8 +6,21 @@ attr_accessor :first_length, :second_length, :third_length
     @third_length = third_length
   end
   
-  def kind 
+  def kind
+    if proper_triangle && greater_thanzero
+      
+    else
+      raise TriangleError
+    end
 end
+
+def proper_triangle
+ @first_length + @second_length < @third_length || @first_length + @third_length < @second_length || @second_length + @third_length < @first_length
+ end
+ 
+ def greater_than_zero
+   @first_length > 0 && @second_length > 0 && @third_length > 0 
+ end
 
 class TriangleError < StandardError
   
